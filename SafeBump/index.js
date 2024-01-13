42969510f440bd5563953b1a4e86dd8656b154d0
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2024-01-13 19:55:00 +0100

    remove dirs

--- SafeBump/index.js
+++
@@ -1,7 +0,0 @@
-// let homeEl = document.getElementById("Home")
-
-// function change(){
-//     homeEl.textContent = "Home"
-//     homeEl.style.color = "red"
-//     homeEl.style.fontWeight = 600
-// }
